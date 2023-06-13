0e149d9adebe3ae40467cd33513a865c03e995df
seunghyun711 <seunghyun711@gmail.com>
2023-06-13 23:05:15 +0900

    level0 - 문자열 출력하기

---
+++ src/Programmers/level0/Solution1.java
@@ -0,0 +1,15 @@
+package Programmers.level0;
+
+import java.util.Scanner;
+
+/*
+<문자열 출력하기>
+문자열 str이 주어질 때, str을 출력하는 코드 작성
+ */
+public class Solution1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println(a);
+    }
+}

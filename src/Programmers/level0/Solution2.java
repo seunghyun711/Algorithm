9b03d6416b2e1bea2770493023c32b219cd6076a
seunghyun711 <seunghyun711@gmail.com>
2023-06-14 21:53:45 +0900

    [programmers] level0 - a,b 출력하기

---
+++ src/Programmers/level0/Solution2.java
@@ -0,0 +1,18 @@
+package Programmers.level0;
+
+import java.util.Scanner;
+
+/*
+<a, b 출력하기?
+정수 a, b가 주어지면 각 수를 입력받아 출력
+ */
+public class Solution2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
